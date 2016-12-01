740fa9133343786d7e43aeeacbca565cc3cc2a29
alan.franco <alandepulofranco@hotmail.com>
2016-12-01 10:15:08 -0200

    Conclusão dos Controladores das Telas (Pagamento, Parcela).

--- WebContent/control/parcela.js
+++ WebContent/control/parcela.js
@@ -1,23 +1,27 @@
-var app = angular.module('alunoModule',[]);
-app.controller('alunoControl',function($scope,$http){
+var app = angular.module('parcelaModule',['angularUtils.directives.dirPagination']);
+app.controller('parcelaControl',function($scope,$http){
 	
-	url = 'http://localhost:8080/DS2016-2Ecommerce/rs/aluno';
+	url = 'http://localhost:8080/DS2016-2Ecommerce/rs/parcela';
 	
 	$scope.salvar = function() {	
-		if ($scope.aluno.idAluno == undefined || $scope.aluno.idAluno == '') {    		
-			$http.post(url,$scope.aluno).success(function(alunosRetorno) {				
-				$scope.alunos.push(alunosRetorno);
-				$scope.novo();
-				$scope.mensagens.push('Aluno salvo com sucesso');
-			}).error(function (erro) {
-				//$scope.mensagens.push('Erro ao salvar Aluno: '+JSON.stringify(erro));
-				$scope.montaMensagemErro(erro.parameterViolations);
-			});
+		$scope.mensagens=[];
+		if ($scope.parcela.idParcela == undefined || $scope.parcela.idParcela == '') {
+			if($scope.formPrincipal.$invalid){
+				$scope.mensagens.push('Campo(s) Obrigatório(s) não Informado(s)!');
+			}else{
+				$http.post(url,$scope.parcela).success(function(parcelasRetorno) {
+					$scope.parcelas.push(parcelasRetorno);
+					$scope.novo();
+					$scope.mensagens.push('Parcela salvo com sucesso!');
+				}).error(function (erro) {
+					$scope.montaMensagemErro(erro.parameterViolations);
+				});
+			}	
 		} else {
-			$http.put(url,$scope.aluno).success(function(aluno) {
+			$http.put(url,$scope.parcela).success(function(parcela) {
 				$scope.pesquisar();
 				$scope.novo();
-				$scope.mensagens.push('Aluno atualizado com sucesso');
+				$scope.mensagens.push('Parcela atualizado com sucesso!');
 			}).error(function (erro) {				
 				$scope.montaMensagemErro(erro.parameterViolations);
 			});
@@ -33,38 +37,45 @@ app.controller('alunoControl',function($scope,$http){
 	}
 	
 	$scope.pesquisar = function() {
-		$http.get(url).success(function (alunos) {
-			$scope.alunos = alunos;
+		$scope.mensagens=[];
+		$http.get(url).success(function (parcelas) {
+			$scope.parcelas = parcelas;
 		}).error(function (erro) {
 			alert(erro);
 		});
 	}
 	
 	$scope.excluir = function() {
-		if ($scope.aluno.idAluno == undefined || $scope.aluno.idAluno == '') {
-			$scope.mensagens.push('Selecione um Aluno');
+		$scope.mensagens=[];
+		if ($scope.parcela.idParcela == undefined || $scope.parcela.idParcela == '') {
+			$scope.mensagens.push('Selecione um Parcela antes de clicar no botão Excluir.');
 		} else {
-			$http.delete(url+"/"+$scope.aluno.idAluno).success(function() {
-				$scope.alunos.splice($scope.alunos.indexOf($scope.aluno), 1);	
+			$http.delete(url+"/"+$scope.parcela.idParcela).success(function() {
+				$scope.parcelas.splice($scope.parcelas.indexOf($scope.parcela), 1);	
 				$scope.novo();
-				$scope.mensagens.push('Aluno excluído com sucesso');
+				$scope.mensagens.push('Parcela excluído com sucesso!');
 			}).error(function (erro) {
-				//$scope.mensagens.push('Erro ao salvar Aluno: '+JSON.stringify(erro));
 				$scope.montaMensagemErro(erro.parameterViolations);
 			});
 		}
 	}
 	
 	$scope.novo = function() {
-		$scope.aluno = {};
+		$scope.parcela = {};
 		$scope.mensagens=[];
 	}
 	
-	$scope.seleciona = function(aluno) {
-		$scope.aluno = aluno;
+	$scope.seleciona = function(parcela) {
+		$scope.mensagens=[];
+		$scope.parcela = parcela;
 	}
 	
 	$scope.pesquisar();
 	$scope.novo();
+	
+	$scope.ordenar = function(keyname){
+        $scope.sortKey = keyname;
+        $scope.reverse = !$scope.reverse;
+    };
 	
 });
